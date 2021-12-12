f63d30c2ccacb3a0864c21d0420328e78ebc3e6b
Александр Немеров <nemerov_a@mail.ru>
2021-12-12 15:47:09 +0800

    ghghghfh

---
+++ PraW_3_Base_progs/Source.cpp
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#define N 10
+#define M 1000
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+#include <time.h>
+#include <Windows.h>
+
+using namespace std;
+
+void main() {
+	setlocale(LC_ALL, "rus");
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	/*Ввести строку с клавиатуры. Проверить, является ли она правильным идентификатором
+	(может содержать цифры, символы латинского алфавита, знаки подчеркивания,
+	начинается с символа подчеркивания или латинского символа).*/
+}
